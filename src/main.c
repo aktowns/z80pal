d62fb191c78076794f9bb4d371f77a3cf697e3b5
Ashley Towns <mail@ashleytowns.id.au>
2019-09-18 13:33:08 +1000

    wip

--- src/main.c
+++ src/main.c
@@ -28,53 +28,85 @@ typedef struct {
 z80_t* new_z80() {
   z80_t* z80 = malloc(sizeof(z80_t));
 
-  uint8_t i = 0;
-  // addressbus
-  for (uint8_t z = 0; z < 16; z++, i++)
-    pin_init(&z80->address_bus[z], &pin_device[i], PIN_INPUT);
-
-  // databus
-  for (uint8_t z = 0; z < 8; z++, i++)
-    pin_init(&z80->data_bus[z], &pin_device[i], PIN_OUTPUT);
-
-  // clock
-  pin_init(&z80->clk, &pin_device[i++], PIN_OUTPUT);
-
-  // system control
-  pin_init(&z80->system_control.m1, &pin_device[i++], PIN_OUTPUT);
-  pin_init(&z80->system_control.mreq, &pin_device[i++], PIN_OUTPUT);
-  pin_init(&z80->system_control.iorq, &pin_device[i++], PIN_OUTPUT);
-  pin_init(&z80->system_control.rd, &pin_device[i++], PIN_OUTPUT);
-  pin_init(&z80->system_control.wr, &pin_device[i++], PIN_OUTPUT);
-  pin_init(&z80->system_control.rfsh, &pin_device[i++], PIN_OUTPUT);
+  uint8_t i = 2;
 
   // cpu control
+  std_printf(FSTR("halt: D%i\n"), i);
   pin_init(&z80->cpu_control.phalt, &pin_device[i++], PIN_OUTPUT);
+  std_printf(FSTR("wait: D%i\n"), i);
   pin_init(&z80->cpu_control.pwait, &pin_device[i++], PIN_OUTPUT);
+  std_printf(FSTR("int: D%i\n"), i);
   pin_init(&z80->cpu_control.pint, &pin_device[i++], PIN_OUTPUT);
+  std_printf(FSTR("nmi: D%i\n"), i);
   pin_init(&z80->cpu_control.pnmi, &pin_device[i++], PIN_OUTPUT);
+  std_printf(FSTR("reset: D%i\n"), i);
   pin_init(&z80->cpu_control.preset, &pin_device[i++], PIN_OUTPUT);
 
   // cpu bus control
+  std_printf(FSTR("busack: D%i\n"), i);
   pin_init(&z80->cpu_bus_control.busack, &pin_device[i++], PIN_OUTPUT);
+  std_printf(FSTR("busrq: D%i\n"), i);
   pin_init(&z80->cpu_bus_control.busrq, &pin_device[i++], PIN_OUTPUT);
+
+  i = 22;
+
+  // addressbus
+  std_printf(FSTR("address bus: D%i"), i);
+  for (uint8_t z = 0; z < 16; z++, i++)
+    pin_init(&z80->address_bus[z], &pin_device[i], PIN_INPUT);
+  std_printf(FSTR("-D%i\n"), i-1);
+
+  // databus
+  std_printf(FSTR("data bus: D%i"), i);
+  for (uint8_t z = 0; z < 8; z++, i++)
+    pin_init(&z80->data_bus[z], &pin_device[i], PIN_OUTPUT);
+  std_printf(FSTR("-D%i\n"), i-1);
+
+  // clock
+  std_printf(FSTR("clk: D%i\n"), i);
+  pin_init(&z80->clk, &pin_device[i++], PIN_OUTPUT);
+
+  // system control
+  std_printf(FSTR("m1: D%i\n"), i);
+  pin_init(&z80->system_control.m1, &pin_device[i++], PIN_OUTPUT);
+  std_printf(FSTR("mreq: D%i\n"), i);
+  pin_init(&z80->system_control.mreq, &pin_device[i++], PIN_OUTPUT);
+  std_printf(FSTR("iorq: D%i\n"), i);
+  pin_init(&z80->system_control.iorq, &pin_device[i++], PIN_OUTPUT);
+  std_printf(FSTR("rd: D%i\n"), i);
+  pin_init(&z80->system_control.rd, &pin_device[i++], PIN_OUTPUT);
+  std_printf(FSTR("wr: D%i\n"), i);
+  pin_init(&z80->system_control.wr, &pin_device[i++], PIN_OUTPUT);
+  std_printf(FSTR("rfsh: D%i\n"), i);
+  pin_init(&z80->system_control.rfsh, &pin_device[i++], PIN_OUTPUT);
+
 
   return z80;
 }
+
+#define CLOCK_SPEED 1000
 
 //static struct event_t clock_event;
 static struct timer_t clock_timer;
 
 static void clock_cb(void *arg_p) {
+ // std_printf(FSTR("tick\n"));
+
   pin_toggle(&((z80_t*)arg_p)->clk);
+
 
   //uint32_t mask = 0x01;
   //event_write_isr(&clock_event, &mask, sizeof(mask));
 }
 
 void write_data(z80_t* z80, uint8_t byte) {
-  for (uint8_t i = 0; i < 8; i++)
-    pin_write(&z80->data_bus[i], (byte >> i) & 1);
+  std_printf(FSTR("writing: "));
+  for (uint8_t i = 0; i < 8; i++) {
+    int status = (byte >> 1) & 1;
+    std_printf(FSTR("%i to pin %i "), status, i);
+    pin_write(&z80->data_bus[i], status);
+  }
+  std_printf(FSTR("\n"));
 }
 
 uint16_t read_address(z80_t* z80) {
@@ -106,24 +138,52 @@ void boot(z80_t* z80) {
   pin_write(&z80->cpu_control.pwait, 1);
   pin_write(&z80->cpu_bus_control.busrq, 1);
 
-  pin_write(&z80->cpu_control.preset, 0);
   thrd_sleep_ms(1000);
+  std_printf(FSTR("rebooting z80\n"));
+  pin_write(&z80->cpu_control.preset, 0);
+  thrd_sleep_ms(CLOCK_SPEED * 5);
   pin_write(&z80->cpu_control.preset, 1);
-
-  clock_start(z80);
 }
 
-#define Z80_NOP = (0b00000000)
+#define Z80_NOP (0b00000000)
 
 int main() {
+  struct uart_driver_t uart;
+
   sys_start();
+
+  uart_module_init();
+  uart_init(&uart, &uart_device[0], 38400, NULL, 0);
+  uart_start(&uart);
+
+  sys_set_stdout(&uart.chout);
+
+  thrd_sleep_ms(1000);
+  std_printf(FSTR("booting z80 cpu\n"));
+
   z80_t* z80 = new_z80();
 
-  boot(z80);
+  clock_start(z80);
 
-  uint16_t addrq = 0;
-  write_data(z80, Z80_NOP)
-  addrq = read_address(z80);
+  boot(z80);
+  write_data(z80, Z80_NOP);
+
+  //uint16_t addrq = 0;
+  //addrq = read_address(z80);
+
+  //std_printf(FSTR("address=0x%08x\n"), addrq);
+
+  int z = 0;
+  while (1) {
+    thrd_sleep_ms(1000);
+
+    if (z == 30) {
+      boot(z80);
+      write_data(z80, Z80_NOP);
+      z = 0;
+    }
+    z++;
+  }
 
   return 0;
 }
