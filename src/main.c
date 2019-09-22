afe220ab35592d5cbbef7fc4aef09917f9750142
Ashley Towns <mail@ashleytowns.id.au>
2019-09-22 23:55:34 +1000

    wip

--- src/main.c
+++ src/main.c
@@ -12,7 +12,7 @@
 #define TICK_SPEED 63974
 
 #define write_data(byte) (PORTL = byte)
-#define read_address() ((uint16_t)(PINA | (PINC << 8)))
+#define read_address() (((uint16_t)PINA << 8u) | PINC)
 
 #define ADDR_A_0  PA0   // D22
 #define ADDR_A_1  PA1   // D23
@@ -40,23 +40,23 @@
 #define DATA_L_6  PL1   // D48
 #define DATA_L_7  PL0   // D49
 
-#define HALT_E    PE4   // D2
-#define WAIT_E    PE5   // D3
-#define INT_G     PG5   // D4
-#define NMI_E     PE3   // D5
-#define RESET_H   PH3   // D6
+#define HALT_E    (unsigned)PE4   // D2
+#define WAIT_E    (unsigned)PE5   // D3
+#define INT_G     (unsigned)PG5   // D4
+#define NMI_E     (unsigned)PE3   // D5
+#define RESET_H   (unsigned)PH3   // D6
 
-#define M1_H      PH4   // D7
-#define MREQ_H    PH5   // D8
-#define IORQ_H    PH6   // D9
-#define RD_B      PB4   // D10
-#define WR_B      PB5   // D11
-#define RFSH_B    PB6   // D12
+#define M1_H      (unsigned)PH4   // D7
+#define MREQ_H    (unsigned)PH5   // D8
+#define IORQ_H    (unsigned)PH6   // D9
+#define RD_B      (unsigned)PB4   // D10
+#define WR_B      (unsigned)PB5   // D11
+#define RFSH_B    (unsigned)PB6   // D12
 
-#define CLK_B     PB7   // D13
+#define CLK_B     (unsigned)PB7   // D13
 
-#define BUSRQ_B   PB1   // D52
-#define BUSACK_B  PB0   // D53
+#define BUSRQ_B   (unsigned)PB1   // D52
+#define BUSACK_B  (unsigned)PB0   // D53
 
 #define Z80_NOP (0b00000000)
 
@@ -71,45 +71,55 @@ void setup_z80() {
   DDRC = 0;
 
   // Setup data bus
-  DDRL = UINT8_MAX;
+  DDRL = 0b11111111;
 
   // Setup CPU Control
-  DDRE |= (0<<HALT_E)|(1<<WAIT_E)|(1<<NMI_E);
-  DDRG |= (1<<INT_G);
-  DDRH |= (1<<RESET_H);
+  DDRE &= ~(1u << HALT_E);
+  DDRE |= (1u << WAIT_E)
+       |  (1u << NMI_E);
+  DDRG |= (1u << INT_G);
+  DDRH |= (1u << RESET_H);
 
   // Setup CPU bus control
-  DDRB |= (0<<BUSACK_B)|(1<<BUSRQ_B);
+  DDRB &= ~(1u << BUSACK_B);
+  DDRB |= (1u << BUSRQ_B);
 
   // Setup Clock
-  DDRB |= (1<<CLK_B);
+  DDRB |= (1u << CLK_B);
 
   // Setup system control
-  DDRH |= (0<<M1_H)|(0<<MREQ_H)|(0<<IORQ_H);
-  DDRB |= (0<<RD_B)|(0<<WR_B)|(0<<RFSH_B);
+  DDRH &= ~((1u << M1_H)
+           |(1u << MREQ_H)
+           |(1u << IORQ_H));
+  DDRB &= ~((1u << RD_B)
+           |(1u << WR_B)
+           |(1u << RFSH_B));
 }
 
 static int tick_count = 0;
 
 ISR (TIMER1_OVF_vect) {
   tick_count++;
-  if (tick_count > 10) {
-    PORTB ^= (1 << CLK_B);
+  if (tick_count > 5) {
+    PORTB ^= (1u << CLK_B);
     tick_count = 0;
   }
   TCNT1 = TICK_SPEED;
 }
 
 void boot() {
-  PORTG |= (1<<INT_G);
-  PORTE |= (1<<NMI_E)|(1<<WAIT_E);
-  PORTB |= (1<<BUSRQ_B);
+  PORTG |= (1u << INT_G);
+  PORTE |= (1u << NMI_E)
+        |  (1u << WAIT_E);
+  PORTB |= (1u << BUSRQ_B);
 
+  PORTH |= (1u << RESET_H);
   _delay_ms(1000);
-
-  PORTH |= (0<<RESET_H);
-  _delay_ms(CLOCK_SPEED * 5);
-  PORTH |= (1<<RESET_H);
+  printf_P(PSTR("Rebooting.."));
+  PORTH &= ~(1u << RESET_H);
+  _delay_ms(CLOCK_SPEED * 15);
+  PORTH |= (1u << RESET_H);
+  printf_P(PSTR("Rebooted..\n"));
 }
 
 
@@ -122,13 +132,13 @@ int uart_putchar(char c, FILE *stream) {
   if (c == '\n')
     uart_putchar('\r', stream);
 
-  loop_until_bit_is_set(UCSR0A, UDRE0);
+  loop_until_bit_is_set(UCSR0A, UDRE0); // NOLINT(hicpp-signed-bitwise)
   UDR0 = c;
 
   return 0;
 }
 
-FILE uart_str = FDEV_SETUP_STREAM(uart_putchar, NULL, _FDEV_SETUP_WRITE);
+FILE uart_str = FDEV_SETUP_STREAM(uart_putchar, NULL, _FDEV_SETUP_WRITE); // NOLINT(cert-fio38-c,misc-non-copyable-objects)
 
 int main() {
   // UART setup
@@ -146,8 +156,9 @@ int main() {
   TCCR1B = 0x00;
 
   TCNT1 = TICK_SPEED;
-  TCCR1B |= (1 << CS10) | (1 << CS12);
-  TIMSK1 |= (1 << TOIE1);
+  TCCR1B |= (1u << (unsigned)CS10)
+         |  (1u << (unsigned)CS12);
+  TIMSK1 |= (1u << (unsigned)TOIE1);
   sei();
 
   setup_z80();
@@ -157,14 +168,36 @@ int main() {
 
   uint16_t addrq = 0;
   addrq = read_address();
-  printf_P(PSTR("address=0x%02x\n"), addrq);
+  printf_P(PSTR("address=0x%04x\n"), addrq);
 
   int z = 0;
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wmissing-noreturn"
   while (1) {
     _delay_ms(1000);
 
-    addrq = read_address();
-    printf_P(PSTR("address=0x%02x\n"), addrq);
+    printf_P(PSTR("address: "));
+    for (int i = 0; i < 8; i++) {
+      if (bit_is_set(PINA, i)) {
+        printf_P(PSTR("1"));
+      } else {
+        printf_P(PSTR("0"));
+      }
+    }
+    printf_P(PSTR("_"));
+    for (int i = 0; i < 8; i++) {
+      if (bit_is_set(PINC, i)) {
+        printf_P(PSTR("1"));
+      } else {
+        printf_P(PSTR("0"));
+      }
+    }
+    printf_P(PSTR("\n"));
+
+
+    //addrq = read_address();
+    //printf_P(PSTR("address=0x%04x\n"), addrq);
 
     if (z == 30) {
       boot();
@@ -173,6 +206,7 @@ int main() {
     }
     z++;
   }
+#pragma clang diagnostic pop
 
   return 0;
 }
